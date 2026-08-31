bffd293d68475c79d959dd59e59244778fcdc4bb
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-08-31 20:08:03 +0600

    Create 2.7_stripping_names.py

---
+++ 02_variables_and_simple_data_types/strings/labs/2.7_stripping_names.py
@@ -0,0 +1,13 @@
+person_name = "\t\nAnkita Modak\n\t"
+
+print("Orginal (with whitespace):")
+print(person_name)
+
+print("Using lstrip method :")
+print(person_name.lstrip())
+
+print("Using rstrip method :")
+print(person_name.rstrip())
+
+print("Using strip() method :")
+print(person_name.strip())
