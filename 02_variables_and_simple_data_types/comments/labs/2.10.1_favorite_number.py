a7179e2e6e6096432c50992e1a7ae9eb1e7a87bc
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-02 22:53:56 +0600

    Update 2.10.1_favorite_number.py

--- 02_variables_and_simple_data_types/comments/labs/2.10.1_favorite_number.py
+++ 02_variables_and_simple_data_types/comments/labs/2.10.1_favorite_number.py
@@ -1,1 +1,10 @@
+# Author: Developer
+# Date: 2026-09-02
+# This program stores a favorite number in a variable and prints a message revealing it.
 
+favorite_number = 13
+
+# Convert the integer to a string using str() and concatenate it into a message
+message = "My favorite number is " + str(favorite_number) + "."
+
+print(message)
