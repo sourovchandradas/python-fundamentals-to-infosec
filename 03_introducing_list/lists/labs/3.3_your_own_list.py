e14205788f3b9fdb502db3859fefd953091ddcc7
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-03 23:46:57 +0600

    Upload 3.3_your_own_list.py

--- 03_introducing_list/lists/labs/3.3_your_own_list.py
+++ 03_introducing_list/lists/labs/3.3_your_own_list.py
@@ -1,1 +1,8 @@
+# 3-3. Your Own List (your_own_list.py)
+# Stores preferred mode of transportation and print statements about them.
 
+transportation = ['Royal Enfield', 'Mercedes-Benz', 'Nissan', 'TATA']
+print(f"I would love to go on long rides with a {transportation[0]}.")
+print(f"My ultimate luxury car choice is a {transportation[1]}.")
+print(f"I admire the reliability of a {transportation[2]}.")
+print(f"A {transportation[3]} is a great choice for family trips.")
