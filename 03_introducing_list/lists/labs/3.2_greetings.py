4fd8492483a7e29b67bd439a1ea6f321b77a12f2
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-03 23:29:06 +0600

    Upload and rename 3.1_greetings.py to 3.2_greetings.py

---
+++ 03_introducing_list/lists/labs/3.2_greetings.py
@@ -0,0 +1,10 @@
+# 3-2. Greetings (greetings.py)
+# Print a personalized greeting to each person in the list.
+
+names = ['Smith', 'Miller', 'Neesham', 'Stokes', 'Hardik']
+
+print(f"Hello {names[0]}, hope you are having a great day!")
+print(f"Hello {names[1]}, hope you are having a great day!")
+print(f"Hello {names[2]}, hope you are having a great day!")
+print(f"Hello {names[3]}, hope you are having a great day!")
+print(f"Hello {names[4]}, hope you are having a great day!")
