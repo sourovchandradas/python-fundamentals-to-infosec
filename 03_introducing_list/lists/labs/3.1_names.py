5b7ed8b17d2aae2464964bc5e3cf66704619f1f3
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-03 23:20:44 +0600

    Upload 3.1_names.py

--- 03_introducing_list/lists/labs/3.1_names.py
+++ 03_introducing_list/lists/labs/3.1_names.py
@@ -1,1 +1,10 @@
+# 3-1. Names (names.py)
+# Store friend's names in a list and print each name individually.
 
+names = ['Smith', 'Miller', 'Neesham', 'Stokes', 'Hardik']
+
+print(names[0]) # Output: Smith
+print(names[1]) # Output: Miller
+print(names[2]) # Output: Neesham
+print(names[3]) # Output: Stokes
+print(names[4]) # Output: Hardik
