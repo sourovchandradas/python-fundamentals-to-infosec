c14a90c89a0c80a0bcb764ca66a254fb967de600
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-05 16:22:38 +0600

    upload 3.4_guest_list.py

--- 03_introducing_list/modifying_elements/labs/3.4_guest_list.py
+++ 03_introducing_list/modifying_elements/labs/3.4_guest_list.py
@@ -1,1 +1,7 @@
+# Create an initial guest list
+guests = ['virat kohli', 'leonel messi', 'cristiano ronaldo']
 
+# Send personalized invitations
+print(f"Dear {guests[0].title()}, you are invited to the dinner!")
+print(f"Dear {guests[1].title()}, you are invited to the dinner!")
+print(f"Dear {guests[2].title()}, you are invited to the dinner!")
