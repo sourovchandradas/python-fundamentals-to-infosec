5082e3818389bb0a2c41fc45c87b4b14879e8333
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-05 16:57:27 +0600

    Create 3.9_dinner_guests.py and upload code

---
+++ 03_introducing_list/organizing_lists/labs/3.9_dinner_guests.py
@@ -0,0 +1,4 @@
+# Create an initial guest list
+guests = ['virat kohli', 'leonel messi', 'novak djokovic', 'ms dhoni', 'sergio ramos', 'michael phelps']
+
+print(len(guests))  # Print the number of guests
