d0ac86ee4a26902b613d1d78eaf714798f4f4565
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-07 23:40:44 +0600

    Upload and rename 4-01.pizzas.py to 4-01.favorite_pizzas.py

---
+++ 04_working_with_lists/looping_through_an_entire_list/labs/4-01.favorite_pizzas.py
@@ -0,0 +1,11 @@
+# Exercise 4-01: Favorite Pizza
+# Demonstrating list iteration using a for loop.
+
+pizzas = ['pepperoni', 'margherita', 'hawaiian', 'veggie']
+
+# Print a personalized statement for each pizza type
+for pizza in pizzas:
+    print(f"I really love {pizza.title()} pizza!")
+
+# Post-loop summary statement
+print("\nI really love all these types of pizza!")
