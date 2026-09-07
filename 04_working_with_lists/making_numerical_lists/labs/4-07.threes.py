868de82aa8aba7003c7efb63c264b7cea5eb372d
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-08 00:15:00 +0600

    Update and rename 4-06.threes.py to 4-07.threes.py

---
+++ 04_working_with_lists/making_numerical_lists/labs/4-07.threes.py
@@ -0,0 +1,7 @@
+# Exercise 4-7: Threes
+# Creating a list of multiples of 3 from 3 to 30 and printing each item using a for loop.
+
+threes = list(range(3, 31, 3))
+
+for number in threes:
+    print(number)
