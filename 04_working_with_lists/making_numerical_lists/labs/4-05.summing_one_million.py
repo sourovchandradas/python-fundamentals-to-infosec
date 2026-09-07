cc8b25737545dbf53243394834f000fbe326fd7b
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-08 00:07:15 +0600

    Create 4-05.summing_one_million.py upload code

---
+++ 04_working_with_lists/making_numerical_lists/labs/4-05.summing_one_million.py
@@ -0,0 +1,16 @@
+# Exercise 4-04: One Million
+# Generating a list of numbers from 1 to 1 million and printing them.
+
+numbers = list(range(1, 1000001))
+
+for number in numbers:
+    print(number)
+
+# Find minimum number in the list using min() function
+print("Minimum number:", min(numbers))
+
+# Find maximum number in the list using max() function 
+print("Maximum number:", max(numbers))
+
+# Calculate total sum in the list using sum() function
+print("Total sum:", sum(numbers))
