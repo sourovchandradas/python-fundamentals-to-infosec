de51d2ec6e37c621a7b8f7ef1e6220ba902976b4
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-08 09:21:44 +0600

    Update 4-08.cubes.py

--- 04_working_with_lists/making_numerical_lists/labs/4-08.cubes.py
+++ 04_working_with_lists/making_numerical_lists/labs/4-08.cubes.py
@@ -1,1 +1,10 @@
+# Exercise 4-08: Cubes
+# Creating a list of the first 10 cubes using a loop and printing each value.
 
+cubes = []
+for number in range(1, 11):
+    cube_value = number ** 3
+    cubes.append(cube_value)
+
+for cube in cubes:
+    print(cube)
