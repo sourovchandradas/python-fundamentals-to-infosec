0a301ac95392e672380f78a9a744517608baebf6
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-09 19:30:23 +0600

    Update 4-10.slices.py

--- 04_working_with_lists/slicing_a_list/labs/4-10.slices.py
+++ 04_working_with_lists/slicing_a_list/labs/4-10.slices.py
@@ -1,1 +1,18 @@
+# Exercise 4-10: Slices
 
+pizzas = ['pepperoni', 'margherita', 'hawaiin', 'veggie', 'bbq chicken']
+
+# Print the first three pizzas in the list
+print("The first three pizzas in the list are:")
+for pizza in pizzas[:3]:
+    print(pizza)
+
+# Print three pizzas from the middle of the list
+print("\nThree pizzas from the middle of the list are:")
+for pizza in pizzas[1:4]:
+    print(pizza)
+
+# Print the last three pizzas in the list are
+print("\nThe last three pizzas in the list are:")
+for pizza in pizzas[-3:]:
+    print(pizza)
