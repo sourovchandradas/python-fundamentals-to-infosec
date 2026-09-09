495323c7abe4a25dbbd1ec3eaabfaf5070863dda
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-09 21:42:59 +0600

    Update 4-12.more_loops.py and upload

--- 04_working_with_lists/slicing_a_list/labs/4-12.more_loops.py
+++ 04_working_with_lists/slicing_a_list/labs/4-12.more_loops.py
@@ -1,1 +1,12 @@
+# Exercise 4-12: More Loops
 
+my_pizzas = ['pizza', 'falafel', 'carrot cake', 'cannoli']
+friend_food = ['pizza', 'falafel', 'carrot cake', 'ice cream']
+
+print("My favorite foods are:")
+for food in my_pizzas:
+    print(food.title())
+
+print("\nMy friend's favorite foods are:")
+for food in friend_food:
+    print(food.title())
