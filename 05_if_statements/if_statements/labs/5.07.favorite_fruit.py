7a610dbb79449e197919231ae7b8e89da6928244
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-12 12:02:19 +0600

    Create 5.07.favorite_fruit.py and upload code

---
+++ 05_if_statements/if_statements/labs/5.07.favorite_fruit.py
@@ -0,0 +1,14 @@
+# Exercise 5-7: Favorite Fruit
+
+favorite_fruits = ['strawberries', 'bananas', 'papayas']
+
+if 'strawberries' in favorite_fruits:
+    print("You are really like strawberries!")
+if 'apple' in favorite_fruits:
+    print("Apple")
+if 'bananas' in favorite_fruits:
+    print("Your are really like bananas")
+if 'oranges' in favorite_fruits:
+    print("You are really like oranges!")
+if 'papayas' in favorite_fruits:
+    print("You are really like papayas!")
