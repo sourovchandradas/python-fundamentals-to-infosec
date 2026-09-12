fedeed1a7340136c1e52822d8548a92646016d43
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-12 11:13:27 +0600

    Create 5-04.alien_colors_02.py and upload code

---
+++ 05_if_statements/if_statements/labs/5-04.alien_colors_02.py
@@ -0,0 +1,18 @@
+# Exercise 5-4: Alien Colors #2
+
+# Version 1: Runs the if block (Alien color is green)
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points!")
+
+
+# Version 2: Runs the else block (Alien color is not green)
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points!")
