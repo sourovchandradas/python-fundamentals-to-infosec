ae3d8136ea5f1be5040b0fe6c2dac3c0800b1a19
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-12 11:04:36 +0600

    upload code and rename 5-03.alien_colours_01.py to 5-03.alien_colors_01.py

---
+++ 05_if_statements/if_statements/labs/5-03.alien_colors_01.py
@@ -0,0 +1,14 @@
+# Exercise 5-3: Alien Colors #1
+
+# Verion 1: Passes the if test
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+
+# Version 2: Fails the if test
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
