82fc2178edcd0dec55484a579726dcef5004476c
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-12 12:18:55 +0600

    Upload code 5-08.hello_admin.py

--- 05_if_statements/if_statements_with_lists/labs/5-08.hello_admin.py
+++ 05_if_statements/if_statements_with_lists/labs/5-08.hello_admin.py
@@ -1,1 +1,9 @@
+# Exercise 5-8: Hello Admin
 
+usernames = ['admin', 'hardik', 'iyer', 'samson','kartik','siraj']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username.title()}, thank you for logging in again.")
