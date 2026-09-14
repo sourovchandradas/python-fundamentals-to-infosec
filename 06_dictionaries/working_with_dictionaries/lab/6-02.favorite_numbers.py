6b9a2f1eb9faba8d1ebaa34f635c42fc85267003
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-14 19:25:06 +0600

    Update 6-02.favorite_numbers.py

--- 06_dictionaries/working_with_dictionaries/lab/6-02.favorite_numbers.py
+++ 06_dictionaries/working_with_dictionaries/lab/6-02.favorite_numbers.py
@@ -1,1 +1,13 @@
+# 6-02: Favorite Numbers
 
+favorite_numbers = {
+    'rahul': 1,
+    'dhoni': 7,
+    'milar': 10,
+    'russel':12,
+    'du plesis':13
+}
+
+# Iterate through each key-value pair and print the formatted message
+for name, number in favorite_numbers.items():
+    print(f"{name.title()}'s favorite number is {number}.")
