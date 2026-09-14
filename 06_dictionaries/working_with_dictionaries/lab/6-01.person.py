d87024be99da4bbd5f73010cc05eaa1abb688a61
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-14 19:15:02 +0600

    Update 6-01.person.py

--- 06_dictionaries/working_with_dictionaries/lab/6-01.person.py
+++ 06_dictionaries/working_with_dictionaries/lab/6-01.person.py
@@ -1,1 +1,8 @@
+# 6-01: Person
 
+person = {'first_name': 'Shreyas', 'last_name': 'Iyer', 'age': 32, 'city': 'Mumbai'}   # Declare Dictionaries
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
