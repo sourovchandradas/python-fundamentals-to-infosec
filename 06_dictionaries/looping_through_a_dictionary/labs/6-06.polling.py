5fb6dfba7a5e9eabc6321a0f739f44f93e13f50e
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-17 23:49:40 +0600

    Upload 6-06.polling.py

--- 06_dictionaries/looping_through_a_dictionary/labs/6-06.polling.py
+++ 06_dictionaries/looping_through_a_dictionary/labs/6-06.polling.py
@@ -1,1 +1,18 @@
+# Exercise 6-06: Polling
 
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'bash'
+}
+
+# List of people who should take the poll
+people_to_poll= ['jen', 'edward','matt','sarah','elena']
+
+# Loop of people who should take the poll
+for person in people_to_poll:
+    if person in favorite_languages:
+        print(f"Thank you for responding, {person.title()}!")
+    else:
+        print(f"Hi {person.title()}, please take our favorite languages poll!")
