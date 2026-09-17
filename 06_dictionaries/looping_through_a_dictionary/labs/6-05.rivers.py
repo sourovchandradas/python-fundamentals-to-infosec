8d53aa9762e9fc2b711900d5b904c7de72abb1ba
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-17 23:04:30 +0600

    Upload 6-05.rivers.py

--- 06_dictionaries/looping_through_a_dictionary/labs/6-05.rivers.py
+++ 06_dictionaries/looping_through_a_dictionary/labs/6-05.rivers.py
@@ -1,1 +1,21 @@
+# Exercise 6-05: Rivers
 
+rivers = {
+    'nile': 'egypt',
+    'ganga': 'india',
+    'amazon': 'brazil'
+}
+
+# 1. Loop to print a sentence about each river
+for river,country in rivers.items():
+    print(f"{river.title()} runs through the {country.title()}")
+
+# 2. Loop to print the name of each river
+print("\n--- Rivers ---")
+for river in rivers.keys():
+    print(river.title())
+
+# 3. Loop to print the name of each counrty
+print("\n--- Countries ---")
+for country in rivers.values():
+    print(country.title())
