279cc23fc8cf806e48986a037902d2db7f307633
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-18 19:30:03 +0600

    Upload 6-09.favorite_places.py

--- 06_dictionaries/nesting/labs/6-09.favorite_places.py
+++ 06_dictionaries/nesting/labs/6-09.favorite_places.py
@@ -1,1 +1,14 @@
+# Exercise 6-09: Favorite Places
 
+# Create a dictionary with names as keys and a list of favorite places as values
+favorite_places = {
+    'deepti': ['ahemedabad', 'kanpur', 'jaipur'],
+    'mitali': ['hyderabad', 'kolkata', 'dehradun'],
+    'smrity': ['bengalore', 'guwahati', 'lucknow']
+}
+
+# Loop through the dictionary
+for name, places in favorite_places.items():
+    print(f"\n{name.title()}'s favorite places are:")
+    for place in places:
+        print(f"- {place.title()}")
