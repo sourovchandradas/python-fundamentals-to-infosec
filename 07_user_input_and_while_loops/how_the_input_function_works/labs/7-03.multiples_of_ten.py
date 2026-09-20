138b21cd598298d7da8999d145a6a3aecf6c8b3f
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-20 21:31:21 +0600

    Upload code 7-03.multiples_of_ten.py

--- 07_user_input_and_while_loops/how_the_input_function_works/labs/7-03.multiples_of_ten.py
+++ 07_user_input_and_while_loops/how_the_input_function_works/labs/7-03.multiples_of_ten.py
@@ -1,1 +1,9 @@
+# Exercise 7-03: Multiples of Ten
 
+# User input take
+number = int(input("Enter your number: "))
+
+if number % 10 == 0:
+    print(f"{number} is a multiple of 10.")
+else:
+    print(f"{number} is not a multiple of 10.")
