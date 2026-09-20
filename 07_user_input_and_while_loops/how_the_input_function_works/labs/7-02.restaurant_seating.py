70e7fca1de332f99b8b61f0f04b0a2c7c022a60d
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-20 21:25:08 +0600

    Upload 7-02.restaurant_seating.py

--- 07_user_input_and_while_loops/how_the_input_function_works/labs/7-02.restaurant_seating.py
+++ 07_user_input_and_while_loops/how_the_input_function_works/labs/7-02.restaurant_seating.py
@@ -1,1 +1,12 @@
+# Exercise 7-02: Restaurant Seating
 
+# User input
+people_number = input("How many people are in your dinner group? ")
+
+# String input convert into integer value.
+people_number = int(people_number) 
+
+if people_number > 8:
+    print("Your group wait for a table.")
+else:
+    print("Your group table is ready.")
