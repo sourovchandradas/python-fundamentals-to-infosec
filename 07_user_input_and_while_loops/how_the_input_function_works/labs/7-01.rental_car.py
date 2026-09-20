e668d0fa06388fe470a57790a457dbe521e2f494
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-20 20:59:04 +0600

    Upload code 7-01.rental_car.py

--- 07_user_input_and_while_loops/how_the_input_function_works/labs/7-01.rental_car.py
+++ 07_user_input_and_while_loops/how_the_input_function_works/labs/7-01.rental_car.py
@@ -1,1 +1,4 @@
+# Exercise 7-01: Rental Car
 
+favorite_car = input("What kind of rental car would you like? ")     # User input take
+print(f"Let me see if I can find you a {favorite_car.title()}")
