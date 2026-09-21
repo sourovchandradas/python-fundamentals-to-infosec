495a096847842cfe634797ebb47b07b5652202c9
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-21 07:44:02 +0600

    Upload 7-04.pizza_toppings.py

--- 07_user_input_and_while_loops/introducing_while_loops/labs/7-04.pizza_toppings.py
+++ 07_user_input_and_while_loops/introducing_while_loops/labs/7-04.pizza_toppings.py
@@ -1,1 +1,12 @@
+# Exercise 7-04: Pizza Toppings
 
+prompt = "\nPlease enter a pizza topping you'd like:"
+prompt += "\nEnter 'quit' when you are finished: "
+
+while True:
+    topping = input(prompt)
+    
+    if topping.lower() != 'quit':
+        print(f"I'll add {topping} to your pizza!")
+    else:
+        break
