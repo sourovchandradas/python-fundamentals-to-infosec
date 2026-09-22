087a3330b6dceee7800d6b61901c4eb4b34d33a7
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-22 07:06:40 +0600

    Upload 7-08.deli.py

--- 07_user_input_and_while_loops/while_loop_with_lists_and_dictionaries/labs/7-08.deli.py
+++ 07_user_input_and_while_loops/while_loop_with_lists_and_dictionaries/labs/7-08.deli.py
@@ -1,1 +1,18 @@
+# Exercise 7-8: Deli
 
+#List of sandwich orders
+sandwich_orders = ['tuna', 'turkey', 'club', 'chicken', 'veggie']
+
+# Empty list for finished sandwiches
+finished_sandwiches = []
+
+# Process each sandwich order
+while sandwich_orders:
+    current_finished = sandwich_orders.pop(0)
+    print(f"I made your {current_finished} sandwich.")
+    finished_sandwiches.append(current_finished)
+
+# Print all finished sandwiches
+print("\nThe following sandwiches have been made:")
+for sandwich in finished_sandwiches:
+    print(f"- {sandwich.title()} sandwich")
