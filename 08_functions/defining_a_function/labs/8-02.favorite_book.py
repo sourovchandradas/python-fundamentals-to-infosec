d9793c6353206382d3a131a2bc5e4d04ffb2e59a
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-23 08:50:16 +0600

    Update 8-02.favorite_book.py

--- 08_functions/defining_a_function/labs/8-02.favorite_book.py
+++ 08_functions/defining_a_function/labs/8-02.favorite_book.py
@@ -1,1 +1,8 @@
+# Exercise 8-02: Favorite Book
 
+def favorite_book(title):
+    print(f"One of my favorite book is {title}")
+
+# Call the function with a book title as an argument
+book = input("Enter your favorite book name: ")
+favorite_book(book)
