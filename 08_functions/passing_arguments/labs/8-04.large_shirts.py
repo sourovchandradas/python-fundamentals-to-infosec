91b8631b803ca5ce4b642a69e30bbad8591361a4
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-24 22:58:41 +0600

    Upload 8-04.large_shirts.py

--- 08_functions/passing_arguments/labs/8-04.large_shirts.py
+++ 08_functions/passing_arguments/labs/8-04.large_shirts.py
@@ -1,1 +1,13 @@
+# Exercise 8-04: Large Shirts
 
+def make_shirt(size='Large', message='I love Python'):
+    print(f"I'm making a {size}-sized shirt with the message: '{message}'")
+
+# Large shirt with default message
+make_shirt()
+
+# Medium shirt with default message
+make_shirt(size='Medium')
+
+# Custom size shirt with a different message
+make_shirt(size='Small', message='Coding is Awesome!')
