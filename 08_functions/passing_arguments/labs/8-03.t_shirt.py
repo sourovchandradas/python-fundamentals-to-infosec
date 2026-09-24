8fffa76481622cca2755f165fcf6998a26b27345
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-24 22:48:30 +0600

    Upload code 8-03.t_shirt.py

--- 08_functions/passing_arguments/labs/8-03.t_shirt.py
+++ 08_functions/passing_arguments/labs/8-03.t_shirt.py
@@ -1,1 +1,10 @@
+# Exercise 8-03: T-Shirt
 
+def make_shirt(size, message):
+    print(f"I'm making a {size}-sized shirt with the message: '{message}'")
+
+# Call using positional arguments
+make_shirt('Medium', 'Code Is Life')
+
+# Call using keyword arguments
+make_shirt(message='Keep Calm and Python On', size='Large')
