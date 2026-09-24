eacd5e0a5c4414c70830cc79ada714d2f704621b
Sourov Chandra Das <sourovchandradas142@gmail.com>
2026-09-24 23:05:47 +0600

    Create 8-05.cities.py and upload code

---
+++ 08_functions/passing_arguments/labs/8-05.cities.py
@@ -0,0 +1,9 @@
+# Exercise 8-05: Cities
+
+def describe_city(city, country='India'):
+    print(f"{city.title()} is in {country.title()}.")
+
+# Call for three different cities
+describe_city('Bengaluru')
+describe_city('Jaipur')
+describe_city('Dublin', country='Ireland')
